0ad4c1209d673bdf15390b86b2c363cdb2fb66bc
tsindyee <31950666+tsindyee@users.noreply.github.com>
2023-03-07 16:11:17 -0500

    - create simple UI

--- ProjectIgnore/Assets/Script/GameManager.cs
+++ ProjectIgnore/Assets/Script/GameManager.cs
@@ -22,4 +22,17 @@ public class GameManager : MonoBehaviour
         return blink.closed;
     }
 
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null && hit.collider.tag == "Object")
+            {
+                Debug.Log("Object clicked");
+            }
+        }
+    }
+
 }
