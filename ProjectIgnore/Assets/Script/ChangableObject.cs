63dd3bc244774d69c5f8747abea82c8b4b830ed6
tsindyee <31950666+tsindyee@users.noreply.github.com>
2023-03-07 14:20:50 -0500

    - create changable object, update sprite image with duration

---
+++ ProjectIgnore/Assets/Script/ChangableObject.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ChangableObject : MonoBehaviour
+{
+    [SerializeField] List<ObjectInfo> objectInfos;
+    private int currentIndex = 0;
+    private SpriteRenderer render;
+    private float targetDuration = 0;
+    private float currentTimer = 0;
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        render.sprite = objectInfos[currentIndex].image;
+        targetDuration = objectInfos[currentIndex].duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentIndex >= objectInfos.Count)
+        {
+            return; // All Changed
+        }
+        bool shouldUpdate = true; // change to eye blink variable
+        if (shouldUpdate)
+        {
+            currentTimer += Time.deltaTime;
+            if (currentTimer > targetDuration)
+            {
+                currentIndex++;
+                if (currentIndex < objectInfos.Count)
+                {
+                    render.sprite = objectInfos[currentIndex].image;
+                    targetDuration = objectInfos[currentIndex].duration;
+                    currentTimer = 0;
+                }
+            }
+        }
+    }
+}
---
+++ ProjectIgnore/Assets/Script/ObjectInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectInfo 
+{
+    public float duration;
+    public Sprite image;
+}
