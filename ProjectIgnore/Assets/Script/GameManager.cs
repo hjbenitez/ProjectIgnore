3de2b7b51b2c06d1110a2f9fe5090dff75a7e3e9
hjbenitez <humbertobenitez5760@yahoo.ca>
2023-03-07 16:36:04 -0500

    enlarge selected object selected

--- ProjectIgnore/Assets/Script/GameManager.cs
+++ ProjectIgnore/Assets/Script/GameManager.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [SerializeField] private SpriteRenderer item;
     [SerializeField] private GameObject Eyes;
     [SerializeField] private ChangableObject selectedObject;
     private Blink blink;
@@ -15,6 +17,23 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
         blink = Eyes.GetComponent<Blink>();
+        item.gameObject.SetActive(false);
+
+    }
+
+    private void Update()
+    {
+        if(selectedObject != null)
+        {
+            item.gameObject.SetActive(true);
+            item.sprite = selectedObject.GetComponent<SpriteRenderer>().sprite;
+        }
+
+        else
+        {
+            item.sprite = null;
+            item.gameObject.SetActive(false);
+        }
     }
 
     public bool GetEyesIsClosed()
--- ProjectIgnore/Assets/Script/PlayerController.cs
+++ ProjectIgnore/Assets/Script/PlayerController.cs
@@ -15,19 +15,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !objectSelected)
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
 
             if (hit.collider != null && hit.collider.tag == "Object")
             {
                 GameManager.instance.SetSelectedObject(hit.transform.gameObject.GetComponent<ChangableObject>());
+                objectSelected = true;
             }
         }
 
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             GameManager.instance.RemoveSelectedObject();
+            objectSelected = false;
         }
     }
 }
