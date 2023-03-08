c159a435e4bc31b8dddaa068b8533c4cbbc39733
tsindyee <31950666+tsindyee@users.noreply.github.com>
2023-03-08 15:48:47 -0500

    - implement disappear count

--- ProjectIgnore/Assets/Script/ChangableObject.cs
+++ ProjectIgnore/Assets/Script/ChangableObject.cs
@@ -9,10 +9,12 @@ public class ChangableObject : MonoBehaviour
     [SerializeField] List<ObjectInfo> objectInfos;
     private int currentIndex = 0;
     private SpriteRenderer render;
-    private float targetDuration = 0;
-    private float currentTimer = 0;
     private bool isSelected = false;
     private Animator ac;
+
+    [SerializeField] private bool canDisappear = true;
+    private int disappearRequiredCount = 3;
+    private int disappearCounter = 0;
 
     private void Awake()
     {
@@ -23,42 +25,27 @@ public class ChangableObject : MonoBehaviour
     void Start()
     {
         render.sprite = objectInfos[currentIndex].image;
-        targetDuration = objectInfos[currentIndex].duration;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //if (currentIndex >= objectInfos.Count)
-        //{
-        //    return; // All Changed
-        //}
-        //bool shouldUpdate = GameManager.instance.GetEyesIsClosed(); // change to eye blink variable
-        //if (shouldUpdate && isSelected)
-        //{
-        //    currentTimer += Time.deltaTime;
-        //    if (currentTimer > targetDuration)
-        //    {
-        //        currentIndex++;
-        //        ac.SetInteger("index", currentIndex);
-
-        //        if (currentIndex < objectInfos.Count)
-        //        {
-        //            render.sprite = objectInfos[currentIndex].image;
-        //            targetDuration = objectInfos[currentIndex].duration;
-        //            currentTimer = 0;
-        //        }
-        //    }
-        //}
     }
 
     public void UpdatePhrase()
     {
-        currentIndex = isSelected ? currentIndex - 1 : currentIndex + 1;
-        currentIndex = Mathf.Clamp(currentIndex, 0, objectInfos.Count - 1);
-        Debug.Log("currentIndex = " + currentIndex);
-        ac.SetInteger("index", currentIndex);
-        render.sprite = objectInfos[currentIndex].image;
+        if (currentIndex == objectInfos.Count - 1 && !isSelected && canDisappear) // in last phrase and not selected
+        {
+            disappearCounter++;
+            if (disappearCounter >= disappearRequiredCount)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            disappearCounter = 0;
+            currentIndex = isSelected ? currentIndex - 1 : currentIndex + 1;
+            currentIndex = Mathf.Clamp(currentIndex, 0, objectInfos.Count - 1);
+            Debug.Log("currentIndex = " + currentIndex);
+            ac.SetInteger("index", currentIndex);
+            render.sprite = objectInfos[currentIndex].image;
+        }
     }
 
     public void SetIsSelected(bool isSelected)
